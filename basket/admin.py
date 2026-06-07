128fce5b037b9e635bc300f6f1d8828e12075856
Atra-Fartash <atrafartash@gmail.com>
2026-06-07 12:17:25 +0330

    add basket admin

--- basket/admin.py
+++ basket/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from basket.models import Basket, BasketItem, Discount
 
-# Register your models here.
+
+
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ['code', 'discount_type', 'value', 'is_active']
+    list_filter = ['discount_type', 'is_active']
+
+
+admin.site.register(Basket)
+admin.site.register(BasketItem)
+admin.site.register(Discount, DiscountAdmin)
