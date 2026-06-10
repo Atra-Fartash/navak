601c4613a5456c334d17c68cda6e994f42b335d5
Atra-Fartash <atrafartash@gmail.com>
2026-06-10 22:16:21 +0330

    add account models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=11, null=True,blank=True)
+    email = models.EmailField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+
+class OTP(models.Model):
+    otp = models.IntegerField()
+    phone_number = models.CharField(max_length=12)
+    expire_date = models.DateTimeField(null=True, blank=True)
+
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    amount = models.PositiveBigIntegerField()
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.PositiveBigIntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    payment_code = models.CharField(max_length=20)
+    payment_type = models.CharField(max_length=2, choices=[("a", "IN"), ("b", "OUT")])
