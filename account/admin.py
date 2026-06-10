59e999edf7e4538a08a192188475568ae910fa32
Atra-Fartash <atrafartash@gmail.com>
2026-06-10 22:31:55 +0330

    add account admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from account.models import Profile, Wallet, Transaction
 
-# Register your models here.
+
+
+admin.site.register(Profile)
+admin.site.register(Wallet)
+admin.site.register(Transaction)
