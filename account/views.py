0d6b1f6bc601a09b398dc0a814a4f2b0743170c6
Atra-Fartash <atrafartash@gmail.com>
2026-06-13 13:40:03 +0330

    add otp view

--- account/views.py
+++ account/views.py
@@ -1,8 +1,14 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+from rest_framework.views import APIView
 from account.serializers import TransactionSerializer, ProfileSerializer
-from account.models import Transaction, Profile, Wallet
+from account.models import Transaction, Profile, Wallet, OTP
 from rest_framework import permissions
 import random
+from django.core.cache import cache
+from rest_framework.response import Response
+from datetime import timedelta
+from django.utils.timezone import now
+from navak.settings import OTP_LIFETIME
 
 
 
@@ -23,7 +29,37 @@ class ProfileRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
         if user.is_staff:
             return Profile.objects.all()
         return Profile.objects.filter(user=user)
-    
+
+
+class GetOTP(APIView):
+
+    def post(self, request):
+        genrated_otp = random.randint(1000, 9999)
+        phone_number = request.data.get("phone_number")
+        cache.set(phone_number, genrated_otp, timeout=OTP_LIFETIME)
+        otp_object = OTP.objects.create(
+            otp = genrated_otp,
+            phone_number = request.data.get('phone_number'),
+        )
+        # SEND OTP TO USER BY SMS
+        otp_object.expire_date = now() + timedelta(seconds=OTP_LIFETIME)
+        otp_object.save()
+        return Response("OTP sent!")
+
+
+class CheckOTP(APIView):
+
+    def post(self, request):
+        input_otp = request.data.get("otp")
+        input_phone_number = request.data.get("phone_number")
+        saved_otp = cache.get(input_phone_number)
+        saved_otp = OTP.objects.get(phone_number=input_phone_number)
+        if saved_otp.otp == input_otp and saved_otp.expire_date >= now():
+            saved_otp.delete()
+            return Response('OK')
+        else:
+            return Response('Something is wrong!')
+
 
 class TransactionView(CreateAPIView):
     permission_classes = [permissions.IsAdminUser]
--- navak/settings.py
+++ navak/settings.py
@@ -136,3 +136,5 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ]
 }
+
+OTP_LIFETIME = 180
