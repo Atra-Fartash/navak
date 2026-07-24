92ef77f4f5ab456461e846b6b62f7a4eb7b09d1f
Atra-Fartash <atrafartash@gmail.com>
2026-07-24 12:19:26 +0330

    edit account urls

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from account.views import ProfileListCreate, ProfileRetrieveUpdateDestroy, TransactionView, register, index, profile
+from account.views import ProfileListCreate, ProfileRetrieveUpdateDestroy, TransactionView, register, index, profile, RegisterView
 
 
 urlpatterns = [
     path('profile-list-create', ProfileListCreate.as_view()),
     path('profile-retrieve-update-destroy', ProfileRetrieveUpdateDestroy.as_view()),
     path('transaction', TransactionView.as_view()),
+    path('register/', RegisterView.as_view()),
     path('register', register),
     path('index', index),
     path('profile',profile),
